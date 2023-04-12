e68c262067cb1fb78ceacd5fe0a4fcd33ec572eb
natalinesianturi <112567938+natalinesianturi@users.noreply.github.com>
2023-04-13 06:37:47 +0700

    Add files via upload

---
+++ KELILING & LUAS PERSEGI.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main (){
+	int sisi,keliling,luas;
+
+	cout<<"Masukan Sisi Persegi = ";
+	cin>> sisi;
+	keliling=4*sisi;
+	cout<<"Keliling Persegi = "<<keliling<<endl;
+
+	luas=sisi*sisi;
+	cout<<"Luas Persegi = "<<luas;
+	return 0;
+
+}
